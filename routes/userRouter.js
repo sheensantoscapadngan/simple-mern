385791a8d46d62e788936875294c47abfe10ce52
Ezra Magbanua <magbanuaezra@gmail.com>
2022-07-23 23:13:00 +0800

    fix server

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { logIn, signUp, testJWT } from '../controllers/userController.js';
+import { logIn, signUp } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/sign-up', signUp);
 userRouter.post('/login', logIn);
-userRouter.post('/testJWT', testJWT);
 
 export default userRouter;
