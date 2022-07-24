bef988870cc1a50a7d62a09fc5497f4af9bad809
Maxell Milay <milaymaxell@gmail.com>
2022-07-24 14:07:14 +0800

    [Adjustment][Maxell] added back semicolons

--- client/src/pages/DisplayAnimalsPage/DisplayAnimalsPage.js
+++ client/src/pages/DisplayAnimalsPage/DisplayAnimalsPage.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const DisplayAnimalsPage = () => {
-  return <div></div>
-}
+  return <div></div>;
+};
 
-export default DisplayAnimalsPage
+export default DisplayAnimalsPage;
