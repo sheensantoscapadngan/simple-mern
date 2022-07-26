1dd692a9335fa11c05e912c3a45c3301edb54cc3
Maxell Milay <milaymaxell@gmail.com>
2022-07-26 20:34:58 +0800

    [Feature][Maxell] added display animal type feature

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import { StateContext } from '../../App';
 
 const HomePage = () => {
+  const { state } = useContext(StateContext);
+  const [animalTypes, setAnimalTypes] = useState([]);
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${state.token}`,
+    },
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        'http://localhost:5000/api/v1/animals',
+        config
+      );
+      const data = response.data.data.animalList;
+      setAnimalTypes(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>HomePage</h1>
+      <h2>Animal Types:</h2>
+      {animalTypes.map((animalType) => {
+        return (
+          <>
+            <h3>{animalType}</h3>
+          </>
+        );
+      })}
     </div>
   );
 };
