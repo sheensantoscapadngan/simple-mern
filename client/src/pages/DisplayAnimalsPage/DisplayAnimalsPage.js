001220f85260acfb0ca5260bad93b08300131fe4
Maxell Milay <milaymaxell@gmail.com>
2022-07-31 09:42:25 +0800

    [Setup][Maxell] setup fetch data function

--- client/src/pages/DisplayAnimalsPage/DisplayAnimalsPage.js
+++ client/src/pages/DisplayAnimalsPage/DisplayAnimalsPage.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { displayAnimalsPage } from '../../styles/displayAnimalsPage/displayAnimalsPageStyles';
+import { StateContext } from '../../App';
+import axios from 'axios';
 
 const DisplayAnimalsPage = () => {
+  const { state } = useContext(StateContext);
+
+  const axiosConfig = {
+    headers: {
+      Authorization: `Bearer ${state.token}`,
+    },
+  };
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      'http://localhost:5000/api/v1/animals/:animal',
+      axiosConfig
+    );
+    const data = response.data;
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return <Box sx={displayAnimalsPage}>All Animals</Box>;
 };
 
